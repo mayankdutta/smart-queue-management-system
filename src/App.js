d3eb0294dfab7484afb851482341e8ef3155b3bb
mayank dutta <smayankdutt@gmail.com>
2022-06-06 22:59:37 +0530

    some refactoring

--- src/App.js
+++ src/App.js
@@ -1,69 +1,84 @@
-import useHeap from "./Hooks/useHeap"
-import {useEffect, useState} from "react";
+import useHeap from "./Hooks/useHeap";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
 import PrintQueue from "./components/printQueue";
 
 function App() {
-    const [appointments, setAppointments] = useState([
-        {name: "P0", rank: 1, penalty: 1},
-        {name: "P1", rank: 2, penalty: 1},
-        {name: "P2", rank: 3, penalty: 1},
-        {name: "P3", rank: 4, penalty: 1},
-        {name: "P4", rank: 5, penalty: 1},
-        {name: "P5", rank: 6, penalty: 1},
-        {name: "P6", rank: 7, penalty: 1},
-        {name: "P7", rank: 8, penalty: 1},
-        {name: "P8", rank: 9, penalty: 1}
+  const [appointments, setAppointments] = useState([
+    { name: "P0", rank: 1, penalty: 1 },
+    { name: "P1", rank: 2, penalty: 1 },
+    { name: "P2", rank: 3, penalty: 1 },
+    { name: "P3", rank: 4, penalty: 1 },
+    { name: "P4", rank: 5, penalty: 1 },
+    { name: "P5", rank: 6, penalty: 1 },
+    { name: "P6", rank: 7, penalty: 1 },
+    { name: "P7", rank: 8, penalty: 1 },
+    { name: "P8", rank: 9, penalty: 1 },
+  ]);
 
+  const [patientName, setPatientName] = useState("");
+  const [currentPatient, setCurrentPatient] = useState(0);
+
+  const data = useHeap(appointments);
+
+  useEffect(() => { }, [appointments]);
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    let lastElement = parseInt(appointments[appointments.length - 1].rank);
+    setAppointments((prevState) => [
+      ...prevState,
+      { name: patientName, rank: lastElement + 1, penalty: 1 },
     ]);
-    const [patientName, setPatientName] = useState("");
-    const [currentPatient, setCurrentPatient] = useState(0);
+  };
+  const handlePresent = () => {
+    setAppointments((prevState) => data);
+    setAppointments((prevState) =>
+      prevState.filter((value, index) => {
+        return index !== currentPatient;
+      })
+    );
+  };
 
-    const data = useHeap(appointments);
+  const handleAbsent = () => {
+    setAppointments((prevState) => data);
+    setAppointments((prevState) =>
+      prevState.map((value, index) => {
+        return index === currentPatient
+          ? {
+            ...value,
+            rank: value.rank + value.penalty,
+            penalty: value.penalty + 1,
+          }
+          : value;
+      })
+    );
+    // setAppointments(prevState => prevState.filter(value => value.rank !== null));
+    console.log(appointments);
+  };
 
-    useEffect(() => {
-    }, [appointments]);
+  return (
+    <>
+      {/*Initially Appointing the patients*/}
+      <h1>Queue</h1>
+      <PrintQueue data={data} i={currentPatient} />
 
-    const submitForm = (event) => {
-        event.preventDefault();
-        let lastElement = parseInt(appointments[appointments.length - 1].rank);
-        setAppointments(prevState => [...prevState, {name: patientName, rank: lastElement + 1, penalty: 1}]);
-    }
-    const handlePresent = () => {
-        setAppointments(prevState => prevState.filter((value, index) => {
-            return index !== currentPatient
-        }))
-    }
-
-    const handleAbsent = () => {
-        setAppointments(prevState => data);
-        setAppointments(prevState => prevState.map((value, index) => {
-            return (index === currentPatient ? {
-                ...value,
-                rank: value.rank + value.penalty,
-                penalty: value.penalty + 1
-            } : value);
-        }))
-        // setAppointments(prevState => prevState.filter(value => value.rank !== null));
-        console.log(appointments)
-    }
-
-    return <>
-        {/*Initially Appointing the patients*/}
-        <h1>Queue</h1>
-        <PrintQueue data={data} i={currentPatient}/>
-
-        {data.length ?
-            <h5>Turn of patient : {data[currentPatient].name + "     "}
-                <button onClick={handlePresent}>Present</button>
-                <button onClick={handleAbsent}>Absent</button>
-            </h5> :
-            <h5>
-                Empty Clinic.
-            </h5>
-        }
-        <Form patientName={patientName} setPatientName={setPatientName} submitForm={submitForm}/>
-    </>;
+      {data.length ? (
+        <h5>
+          Turn of patient : {data[currentPatient].name + "     "}
+          <button onClick={handlePresent}>Present</button>
+          <button onClick={handleAbsent}>Absent</button>
+        </h5>
+      ) : (
+        <h5>Empty Clinic.</h5>
+      )}
+      <Form
+        patientName={patientName}
+        setPatientName={setPatientName}
+        submitForm={submitForm}
+      />
+    </>
+  );
 }
 
 export default App;
--- src/Hooks/useHeap.jsx
+++ src/Hooks/useHeap.jsx
@@ -1,97 +1,5 @@
 import React, {useEffect, useState} from 'react';
-
-const top = 0;
-
-class PriorityQueue {
-    constructor(comparator = (a, b) => a > b) {
-        this._heap = [];
-        this._comparator = comparator;
-    }
-
-    parent(i) {
-        return ((i + 1) >>> 1) - 1;
-    }
-
-    leftChild(i) {
-        return ((i << 1)) + 1;
-    }
-
-    rightChild(i) {
-        return ((i + 1) << 1);
-    }
-
-    size() {
-        return this._heap.length;
-    }
-
-    isEmpty() {
-        return this.size() === 0;
-    }
-
-    peek() {
-        return this._heap[top];
-    }
-
-    push(...values) {
-        values.forEach((value) => {
-            this._heap.push(value);
-            this._siftUp();
-        });
-        return this.size();
-    }
-
-    pop() {
-        const poppedValue = this.peek();
-        const bottom = this.size() - 1;
-        if (bottom > top) {
-            this._swap(top, bottom);
-        }
-        this._heap.pop();
-        this._siftDown();
-        return poppedValue;
-    }
-
-    replace(value) {
-        const replacedValue = this.peek();
-        this._heap[top] = value;
-        this._siftDown();
-        return replacedValue;
-    }
-
-    _greater(i, j) {
-        return this._comparator(this._heap[i], this._heap[j]);
-    }
-
-    _swap(i, j) {
-        [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
-    }
-
-    _siftUp() {
-        let node = this.size() - 1;
-        while (node > top && this._greater(node, this.parent(node))) {
-            this._swap(node, this.parent(node));
-            node = this.parent(node);
-        }
-    }
-
-    _siftDown() {
-        let node = top;
-        while (
-            (this.leftChild(node) < this.size() && this._greater(this.leftChild(node), node)) ||
-            (this.rightChild(node) < this.size() && this._greater(this.rightChild(node), node))
-            ) {
-            let maxChild =
-                this.rightChild(node) < this.size() &&
-                this._greater(this.rightChild(node), this.leftChild(node))
-                    ? this.rightChild(node)
-                    : this.leftChild(node);
-
-            this._swap(node, maxChild);
-            node = maxChild;
-        }
-    }
-}
-
+import PriorityQueue from "../Algorithm/Heap";
 
 const Heap = (arr) => {
     console.log("printing in the useHeap");
@@ -113,8 +21,8 @@ const Heap = (arr) => {
             const poppedElement = pairwiseQueue.pop();
             data.push({name: poppedElement[0], rank: -1 * poppedElement[1], penalty: poppedElement[2]});
         }
-        return data;
     }
+
     fun();
     return data;
 };
