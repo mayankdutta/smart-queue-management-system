25199b7450512b98ca983a2ec89f9e3f45bca7c1
mayank dutta <smayankdutt@gmail.com>
2022-07-16 11:36:23 +0530

    Linked with backend, issues with cors, ig

---
+++ src/backendData.js
@@ -0,0 +1,3 @@
+export const Backend = {
+    link: "http://localhost:5000",
+};
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,17 @@ import {Link} from "react-router-dom";
 
 function Navbar() {
     return (
-        <>
+        <div style={{display: "flex" , gap: "2rem"}}>
             <Link to={""}>
                 <h4 style={{backgroundColor: "yellow"}}>Option 1</h4>
             </Link>
-        </>
+            <Link to={"/login"}>
+                <h4 style={{backgroundColor: "yellow"}}>Login</h4>
+            </Link>
+            <Link to={"/signup"}>
+                <h4 style={{backgroundColor: "yellow"}}>Signup</h4>
+            </Link>
+        </div>
     );
 }
 
