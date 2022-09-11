650bbe377938e3c13a19dca9f6902d0dd9fe613c
mayank dutta <smayankdutt@gmail.com>
2022-09-12 00:31:40 +0530

    more backend routes created for patients
    integrate them with the frontend

---
+++ Controllers/appointments.js
@@ -0,0 +1,89 @@
+const Patient = require("../Models/Appointments");
+require("dotenv").config()
+
+const registerPatient = async (req, res, next) => {
+    console.warn(req.body)
+
+    try {
+        const patient = new Patient({
+            name: req.body.name,
+            contactNumber: req.body.contactNumber,
+            contactNumberFamilyMember: req.body.contactNumberFamilyMember,
+            Address: req.body.address,
+            doctor: req.body.doctor,
+            registeredBy: req.body.registeredBy,
+            tokenNumber: req.body.tokenNumber,
+            currentPenalty: req.body.currentPenalty,
+        });
+        const result = await patient.save();
+        return res.status(200).send({
+            message: "patient registered successfully",
+            tempResult: {result}
+        });
+    } catch (err) {
+        return res.status(400).send({message: err.message});
+    }
+
+}
+
+const deletePatient = async (req, res, next) => {
+    try {
+        let patient = await Patient.deleteOne({_id: req.params.id})
+        res.status(200).send(patient);
+    } catch (err) {
+        res.status(404).send({
+            message: err
+        });
+    }
+}
+
+const getAllPatient = async (req, res) => {
+    try {
+        const data = await Patient.find();
+        res.status(200).send(data);
+    } catch (err) {
+        res.status(404).send({
+            result: "data not found",
+            message: err
+        })
+    }
+}
+
+const getPatient = async (req, res, next) => {
+    try {
+        const patient = await Patient.find({registeredBy: req.body.registeredBy});
+        res.send(patient);
+    } catch (err) {
+        res.status(401).send({
+            result: "No such record"
+        })
+    }
+}
+
+const putUpdatePatient = async (req, res) => {
+    try {
+        let patient = await Patient.updateOne({_id: req.params.id}, {
+            $set: req.body
+        })
+        res.status(200).send(patient);
+    } catch (err) {
+        res.status(404).send({
+            message: err
+        });
+    }
+}
+
+const getUpdatePatient = async (req, res) => {
+    try {
+        let patient = await Patient.findOne({_id: req.params.id})
+        res.send(patient)
+    } catch (err) {
+        res.status(404).send({
+            message: err,
+            result: "No record found"
+
+        });
+    }
+}
+
+module.exports = {registerPatient, deletePatient, getPatient, getUpdatePatient, putUpdatePatient, getAllPatient}
---
+++ middleware/docdeUser.js
@@ -0,0 +1,11 @@
+const getUserObject = (token) => {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const str = Buffer.from(base64, 'base64').toString('utf8');
+    let jsonPayload = decodeURIComponent((str).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+}
+
+module.exports = getUserObject;
