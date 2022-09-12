c3d346d00c1c8c2bd8031169b1b32d376d1fe46f
mayank dutta <smayankdutt@gmail.com>
2022-09-12 23:25:11 +0530

    more parameters added

--- Models/Appointments.js
+++ Models/Appointments.js
@@ -17,6 +17,26 @@ const AppointmentsSchema = new mongoose.Schema({
     doctor: String,
     registeredBy: String, // logged in register who registered him.
     tokenNumber: String,
+
+    age: String,
+    weight: String,  // can be measured @clinic, if known then filled.
+    bloodPressure: String,  // can be measured @clinic as well as self.
+    bloodType: String,
+    oxygenLevel: String, // can be measured @clinic as well as self.
+    motive: String,
+    ellaborateCaes: String, // to be filled by compounder, adhoc Case, sucide, chakku, banduuk, ladai, accident., ilaaj se phele FIR., NIL Allowed
+    typeOfCase: String,
+    /*
+    1. Consult
+    2. adhocCase,
+    3. Test
+    4. Refer to another.
+    5. Review tests
+    6. Regular mareej, to check progress.
+    7. previous doctor. keep their priority higher then regular.
+
+    assign priority in the following order.
+     */
 })
 
 const Appointments = mongoose.model('Appointments', AppointmentsSchema)
