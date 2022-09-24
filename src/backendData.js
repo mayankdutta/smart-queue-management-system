1272748f96932cfb163a39bbb7e4dabba07112f5
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2022-09-24 23:03:42 +0530

    Update backendData.js

--- src/backendData.js
+++ src/backendData.js
@@ -1,4 +1,4 @@
 export const Backend = {
-    // link: "https://smart-queue-management-system.herokuapp.com",
-    link : "http://localhost:5000",
+    link: "https://smart-queue-management-system.herokuapp.com",
+    // link : "http://localhost:5000",
 };
