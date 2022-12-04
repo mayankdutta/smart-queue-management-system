299eb64fe2e6b55601e47f15d4adbe01be4fc7ac
mayank dutta <smayankdutt@gmail.com>
2022-12-04 16:25:27 +0530

    cors

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const routes = require("./Routes/routes")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const routes = require("./Routes/routes");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
+app.use(cors());
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     console.log("DATABASE CONNECTED");
-}).catch((err) => {
-    console.log("DATABASE NOT CONNECTED")
+  })
+  .catch((err) => {
+    console.log("DATABASE NOT CONNECTED");
     console.log(err);
-})
+  });
 
 app.use("/", routes);
 
@@ -22,6 +31,6 @@ const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
-    console.log(`server is running on port : ${PORT}`)
-    console.log(`server is running on host: ${HOST}`)
-})
+  console.log(`server is running on port : ${PORT}`);
+  console.log(`server is running on host: ${HOST}`);
+});
