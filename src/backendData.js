50d524eb1a49660242f19605d720ea73cd8b8f7e
mayank dutta <smayankdutt@gmail.com>
2022-12-05 01:34:56 +0530

    backend shifted from heroku to cyclic

--- src/backendData.js
+++ src/backendData.js
@@ -1,4 +1,5 @@
 export const Backend = {
-    link: "https://smart-queue-management-system.herokuapp.com",
+    // link: "https://smart-queue-management-system.herokuapp.com",
+  link: "https://repulsive-cod-miniskirt.cyclic.app",
     // link : "http://localhost:5000",
 };
